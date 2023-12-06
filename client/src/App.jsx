e40ace1e5670169ecc8b483a5424a9f3acf48076
sDEV-eloper <thisissurajdev@gmail.com>
2023-12-06 16:22:40 +0530

    created route on pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+
 export default function App() {
+  
   return (
-    <h1 className="text-red-500">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-out" element={<SignOut/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+      <div className='text-black'>
+        {console.log("home page")}
+        <h1>Welcome to the home page!</h1>
+    </div>
+  )
+}
+
