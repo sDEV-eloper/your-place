b41a43752f23cc2cd4956a69af31326dbe02d3c0
sDEV-eloper <thisissurajdev@gmail.com>
2023-12-08 00:32:17 +0530

    now password has been hashed

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,11 +1,12 @@
 import User from '../models/userModel.js'
+import bcryptjs from 'bcryptjs'
 
 export const signup= async(req, res)=>{
 console.log(req.body)
 const {username, email, password} = req.body;
-
+const hashedPassword= bcryptjs.hashSync(password, 10)
 //save to database
-const newUser= new User({username, email, password})
+const newUser= new User({username, email, password:hashedPassword})
 try {
     await newUser.save();
     res.status(201).json("User created successfully");
