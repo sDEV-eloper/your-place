14e4cbba95a87fe8658e9cf82cf5b3b878506324
sDEV-eloper <thisissurajdev@gmail.com>
2023-12-07 13:01:12 +0530

    created basic server

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app=express()
+
+app.get('/', 
+(req,res)=>{
+    res.send('Hello World!')
+    })
+
+const PORT=8080
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
