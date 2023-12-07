545fcbcf9d0737ae1a4c4699480a794e33e0e379
sDEV-eloper <thisissurajdev@gmail.com>
2023-12-08 01:03:39 +0530

    added errorHandler middleware for possible errors

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 
 export const signup= async(req, res, next)=>{
 console.log(req.body)
