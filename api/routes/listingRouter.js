fe25cc8925a96930a296b3c324049e3d9935eb80
sDEV-eloper <thisissurajdev@gmail.com>
2023-12-18 15:20:35 +0530

    added search api route

--- api/routes/listingRouter.js
+++ api/routes/listingRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createList, getList, deleteList, updateList, getListData } from "../controllers/listController.js";
+import { createList, getList, deleteList, updateList, getListData, getAllList } from "../controllers/listController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router=express.Router()
@@ -9,5 +9,6 @@ router.post('/get-list/:id', verifyToken, getList)
 router.delete('/delete-list/:id', verifyToken, deleteList)
 router.post('/update-list/:id', verifyToken, updateList)
 router.get('/get/:id',  getListData)
+router.get('/get',  getAllList)
 
 export default router;
