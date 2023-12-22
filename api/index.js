fe2f493a073d683abfadbc64fcbf571b9eca16a6
sDEV-eloper <thisissurajdev@gmail.com>
2023-12-22 14:54:59 +0530

    fixex index js string

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use("/api/list", listingRouter)
 
 app.use(express.static(path.join(__dirname, '/client/dist')))
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, "client","dist", "index.html"));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 //error handling middleware
